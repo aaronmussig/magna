5be80ffaab3ca91a2e4aadbb037428cc6f2ae03a
Aaron Mussig <aaronmussig@gmail.com>
2022-03-10 13:05:49 +1000

    Removed content for gh-pages.

--- README.md
+++
@@ -1,2 +0,0 @@
-# magna
-The magna CLI package is a collection of commands that I frequently use in bioinformatics and day-to-day life.
--- magna/config.py
+++
@@ -1,9 +0,0 @@
-import os
-import tempfile
-from pathlib import Path
-
-# Persistent cache
-MAGNA_DIR = os.path.join(Path.home(), '.magna')
-
-# Temporary cache
-CACHE_DIR = os.path.join(tempfile.gettempdir(), 'magna')
