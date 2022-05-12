5cf12adcaf0d1da1f19463a63a371993703604cc
Aaron Mussig <aaronmussig@gmail.com>
2022-05-12 13:43:38 +1000

    docs(pypi): Update PyPI URL.

--- magna/__init__.py
+++ magna/__init__.py
@@ -1,6 +1,6 @@
 __title__ = 'magna'
 __description__ = 'Magna is a collection of commands that I frequently use in bioinformatics and day-to-day life.'
-__url__ = 'https://git.microba.core/modules/python/bio-proc-sdk'
+__url__ = 'https://github.com/aaronmussig/magna'
 __version__ = '3.3.0'
 __author__ = 'Aaron Mussig'
 __author_email__ = 'aaronmussig@gmail.com'
