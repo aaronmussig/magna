1c624166a75a27fd92a8cee8537d3a02ccd895ca
Aaron Mussig <aaronmussig@gmail.com>
2022-05-12 14:15:21 +1000

    fix(rq): Update typer config.

--- magna/__main__.py
+++ magna/__main__.py
@@ -1,15 +1,15 @@
 import typer
 
 from magna.cli.ncbi import app as ncbi_app
+from magna.cli.rq import app as rq_app
 
 app = typer.Typer()
 
 app.add_typer(ncbi_app, name='ncbi')
-app.add_typer(ncbi_app, name='rq')
+app.add_typer(rq_app, name='rq')
 
 # Purely for documentation
 typer_click_object = typer.main.get_command(app)
-
 
 if __name__ == "__main__":
     app()
